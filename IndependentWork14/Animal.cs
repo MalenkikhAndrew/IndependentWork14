d5758a6add2f3ac82b406985bf5c392fe0b906e3
Андрей Маленьких <mrmaloy-1992@mail.ru>
2022-10-17 21:15:34 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork14/Animal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo() 
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+
+    }
+    class Cat : Animal
+    {
+        private string voice;
+        private string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name, string voice)
+            : base(name)
+        {
+            this.voice = voice;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+    }
+    class Dog : Animal
+    {
+            private string voice;
+            private string name;
+            public override string Name
+            {
+                get => name;
+                set => name = value;
+            }
+            public Dog(string name, string voice)
+                : base(name)
+            {
+                this.voice = voice;
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Гав");
+            }
+        } 
+    }
+
---
+++ IndependentWork14/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Васька","Мяу");
+           cat.ShowInfo();
+            Dog dog = new Dog("Филька","Гав");
+           dog.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
